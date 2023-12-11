570732683dd0527930c9c3c27013dca20d64d937
taeyeon02 <80195251+taeyeon02@users.noreply.github.com>
2023-12-11 14:36:24 +0900

    Add files via upload

---
+++ review/kogpt_com_train.py
@@ -0,0 +1,143 @@
+import torch
+import json
+import datasets
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
+from tqdm import tqdm
+from utils import convert_xlsx_to_json
+
+
+# 모델을 학습시키는 Class
+class Model_train:
+
+    # ------------------------------------------------------------------------------------------
+    # func name: __init__                                                                
+    # 목적/용도: 클래스를 선언할 때 model_id와 json_file을 넣고 선언 -> 받은 model_id, json_file로 객체의 변수를 초기화                              
+    # Input: model_id, json_file 경로                                                        
+    # Output: X
+    # ------------------------------------------------------------------------------------------
+    def __init__(self, model_id, json_file):
+        self.model_id = model_id
+        self.data = self.load_dataset(json_file)
+        self.model = self.load_model()
+
+
+    # ------------------------------------------------------------------------------------------
+    # func name: load_dataset                                                              
+    # 목적/용도: json_file을 받고 모델에 학습시킬 데이터셋 구축                           
+    # Input: json_file 경로(json_file은 prompt, input, completion으로 구성되어 있음)                                                        
+    # Output: data
+    # ------------------------------------------------------------------------------------------
+    def load_dataset(self, json_file):
+        tokenizer = AutoTokenizer.from_pretrained(self.model_id) # class에 저장된 model_id로 tokenizer를 불러옴
+        tokenizer.pad_token = tokenizer.eos_token # tokenizer의 pad토큰과 eos토큰을 같게 만들어줌
+        a_json = open(json_file, encoding='utf-8-sig') # json 파일을 utf-8-sig 방식으로 인코딩하여 열고 a_json에 저장
+        a_load = json.load(a_json) # a_json(json 형식) -> a_load(Dict) json 형식을 Python의 Dictionary 형태로 변환(List 안에 Dictionary 형태의 객체가 존재)
+        a_load = datasets.Dataset.from_list(a_load) # List를 Dataset Class의 객체로 변환
+        data = datasets.DatasetDict({"train": a_load}) # Dataset -> DatasetDict
+        data = data.map(
+            lambda x: {'text': f"### Instruction(명령어):\n{x['prompt']}\n\n### Input(입력):\n{x['input']}\n\n### Response(응답):\n{x['completion']}"}
+        ) # json_file에서 하나의 객체 당 prompt, input, completion이 존재, x = 하나의 객체를 의미하며 해당 객체의 prompt, input, completion을 위의 형식대로 대입
+        data = data.map(lambda samples: tokenizer(samples["text"]), batched=True) # 위에 만들었던 데이터를 토큰화하여 data 생성
+
+        return data # 만든 data를 반환
+
+
+    # ------------------------------------------------------------------------------------------
+    # func name: load_dataset                                                              
+    # 목적/용도: 클래스를 선언할 때 받았던 model_id로 모델을 로드                          
+    # Input: X                                                       
+    # Output: model
+    # Lora = Low rank adaptation -> 거대 언어 모델의 학습을 적은 자원으로 수행하기 위해 경량화
+    # r: rank의 개수 - 많으면 계산량 증가 & 성능 증가 & 시간 증가, rank가 높아질수록 계산량과 시간은 증가하지만 성능은 증가하지 않을 수 있음
+    # lora_alpha: 가중치 행렬의 크기 조절
+    # r, lora_alpha의 값을 변형시키며 실험하여 성능 증가를 노려볼 수 있음
+    # dropout: 계산 시 5%는 버리고 계산 - 과적합 방지
+    # ------------------------------------------------------------------------------------------
+    def load_model(self):
+        model = AutoModelForCausalLM.from_pretrained(self.model_id, device_map={"": 0}) # 라이브러리에 있는 pretrain 된 모델을 로드
+        model.gradient_checkpointing_enable() # 메모리 사용량 감소 및 모델의 학습 속도 상승을 위한 기술을 활성화하는 함수
+        model = prepare_model_for_kbit_training(model) # kbit_training(모델의 학습 속도 개선)을 위한 모델 준비
+
+        config = LoraConfig(
+            r=8, 
+            lora_alpha=32, 
+            lora_dropout=0.05, 
+            bias="none",
+            task_type="CAUSAL_LM" 
+        )
+
+        model = get_peft_model(model, config) 
+        model.print_trainable_parameters()
+        return model # model을 반환
+
+
+    # ------------------------------------------------------------------------------------------
+    # func name: train_model                                                            
+    # 목적/용도: model을 학습시키는 함수                          
+    # Input: epoch, batch, learning_rate, dir                                            
+    # Output: X
+    # batch - 한 번에 몇 개씩 학습할지
+    # gradient_accumulation_steps - cuda-memory error를 해결하기 위한 기술 이 step * batch size만큼의 batch size 성능이 나타남
+    # epoch - 학습을 몇 번 반복할지
+    # learning_rate - 학습의 세기, 높으면 반영을 많이 함
+    # float_point - 32bit로 표현하던 실수를 16bit로 표현하여 계산 속도를 올림 (성능은 낮아질 수 있음)
+    # logging_steps - logging_steps마다 학습 관련 log를 출력
+    # output_dir - 모델을 저장하는 폴더 경로
+    #
+    # model의 성능에 영향을 주는 parameter들
+    # TrainingArgument의 epoch, batch, learning_rate, fp16, optim, gradient_accumulation_steps
+    # data_collator의 mlm
+    # ------------------------------------------------------------------------------------------
+    def train_model(self, epoch, batch, l_rate, dir):
+        
+        tokenizer = AutoTokenizer.from_pretrained(self.model_id) # class에 저장된 model_id로 tokenizer를 불러옴
+        tokenizer.pad_token = tokenizer.eos_token # tokenizer의 pad토큰과 eos토큰을 같게 만들어줌
+
+        trainer = Trainer(
+            model=self.model, 
+            train_dataset=self.data["train"],
+            args=TrainingArguments(
+                per_device_train_batch_size=batch, 
+                gradient_accumulation_steps=1, 
+                num_train_epochs=epoch, 
+                learning_rate=l_rate, 
+                fp16=True, 
+                logging_steps=200, 
+                output_dir=dir, 
+                optim="paged_adamw_8bit" 
+            ),
+            data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=True), # data의 전처리, mlm = mask language modeling으로 일부를 마스킹하여 학습 진행
+        )
+        self.model.config.use_cache = False # cache를 사용하여 같은 계산을 스킵하기 위함(시간은 줄어드나, 성능 저하 우려 존재) -> False
+        trainer.train() 
+        print('train completed')
+        
+
+# ------------------------------------------------------------------------------------------
+# func name: load_dataset                                                              
+# 목적/용도: learning_rate에 따라 여러 model을 학습시킴                          
+# Input: kogpt_train class, epoch, batch, l_rate_min, l_rate_max, dir(학습한 모델의 저장 경로)                                                      
+# Output: X
+# epoch를 변화시키지 않은 이유는 큰 epoch로 학습시키고 해당 모델의 checkpoint를 불러오면 다른 epoch로 학습한 모델을 불러오는 것과 같은 효과
+# learning_rate는 1e-05 ~ 5e-05를 추천
+# ------------------------------------------------------------------------------------------
+def train_model_multiple(kogpt_train, epoch, batch, l_rate_min, l_rate_max, dir):
+    while l_rate_min <= l_rate_max: # l_rate_min에 1e-05(10^-5)씩 더하며 l_rate_max가 될 때까지 돌림
+        dir + "/" + str(epoch) + "_" + str(l_rate_min) # 모델을 저장할 폴더 경로 설정
+        kogpt_train.train_model(epoch=epoch, batch=batch, l_rate=l_rate_min, dir=dir) # 모델 학습
+        l_rate_min += 1e-05
+
+
+if __name__ == "__main__":
+
+    mode = 3 # 학습 모드 선택 (0: 요약, 1: 핵심구문추출, 2: 감성분석, 3: 전체)
+    
+    file_name = "./dataset/train_data.xlsx" # 학습 데이터로 사용될 데이터 파일의 이름(경로)
+    json_file = "./dataset/train_data.json" # 엑셀 파일을 json 파일로 변경할 때의 이름
+
+    convert_xlsx_to_json(file_name, json_file, mode) # xlsx -> json으로 변경
+
+    kogpt_train = Model_train("rycont/kakaobrain__kogpt-6b-8bit", json_file) # model_id와 json_file을 넣고 Class 생성
+
+    train_model_multiple(kogpt_train, 50, 4, 3e-05, 3e-05, "./model") # 다른 파라미터로 여러 모델 학습
---
+++ review/utils.py
@@ -0,0 +1,126 @@
+import json
+import openpyxl
+import re
+import random
+
+# ------------------------------------------------------------------------------------------
+# func name: convert_xlsx_to_json                                                           
+# 목적/용도: xlsx 파일을 json 파일로 변환                          
+# Input: xlsx 파일의 경로 및 생성할 json 파일을 저장할 경로, 학습 mode                                               
+# Output: X
+# mode : 0 - 요약, 1 - 핵심구문추출, 2 - 감성분석, 3 - 전체
+# xlsx file에는 해당 모드에 맞는 형식으로 데이터가 구성되어 있어야 함.
+# 형식
+# 1. A행에는 input이
+# ------------------------------------------------------------------------------------------
+def convert_xlsx_to_json(file_name, json_file, mode): 
+
+    workbook = openpyxl.load_workbook(file_name)
+
+    sheet = workbook.active
+
+    data = []
+
+    # 요약 프롬포트
+    prompt_summary = "아래의 텍스트는 요약이 필요한 긴 텍스트입니다.\n"\
+                        "아래의 텍스트에서 내용을 간결하게 요약하세요.\n"\
+                        "요약문의 시작은 [ 로, 끝은 ]로 제시하세요."
+    
+    # 핵심 구문 프롬포트
+    prompt_keyphrase = "아래의 텍스트에 대해 가장 핵심이 되는 key noun phrase를 3개 까지만 추출하세요.\n"\
+                        "keyphrase의 개수는 세 개 이내로 제시해야 합니다\n"\
+                        "각 key phrase는 less than fifteen characters의 짧은 noun phrase로 제시되어야 하고, • 로 시작해야 합니다.\n"\
+                        "noun phrase는 '재미있는 강의내용' 과 같이 명사로 사용될 수 있는 구문입니다.\n"\
+                        "keyphrase 결과의 시작은 [ 로, 끝은 ]로 제시하세요."
+    
+    # 감성분석 프롬포트
+    prompt_senti = "아래의 텍스트를 분석하여 긍정, 또는 부정으로 제시하세요.\n"\
+                    "감성분석 결과의 시작은 [ 로, 끝은 ]로 제시하세요."
+
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+
+        if any(val is None for val in row):
+            continue
+
+        if mode == 0:
+            user_input = row[0]
+
+            completion_summary = '[' + row[1] + ']'
+
+            item_summary = {
+                "prompt": prompt_summary,
+                "input": user_input,
+                "completion": completion_summary
+            }
+            data.append(item_summary)
+
+        elif mode == 1:
+            user_input = row[0]
+
+            keywords = row[1].split('• ')
+            keywords = [keyword.strip().replace('\n', '') for keyword in keywords if keyword.strip()]
+            keywords = ["• " + keyword.strip() for keyword in keywords]
+            keywords = [re.sub(r'\s{2,}', ' ', keyword) for keyword in keywords]
+            completion_keyphrase = '[' + str(keywords)[1:-1] + ']'
+
+            item_keyphrase = {
+                "prompt": prompt_keyphrase,
+                "input": user_input,
+                "completion": completion_keyphrase
+            }
+            data.append(item_keyphrase)
+
+        elif mode == 2:
+            user_input = row[0]
+
+            completion_senti = '[' + row[1] + ']'
+
+            item_senti = {
+                "prompt": prompt_senti,
+                "input": user_input,
+                "completion": completion_senti
+            }
+            data.append(item_senti)
+
+        elif mode == 3:
+            user_input = row[0]
+            completion_summary = '[' + row[1] + ']'
+
+            keywords = row[2].split('• ')
+            keywords = [keyword.strip().replace('\n', '') for keyword in keywords if keyword.strip()]
+            keywords = ["• " + keyword.strip() for keyword in keywords]
+            keywords = [re.sub(r'\s{2,}', ' ', keyword) for keyword in keywords]
+            completion_keyphrase = '[' + str(keywords)[1:-1] + ']'
+
+            completion_senti = '[' + row[3] + ']'
+
+            item_summary = {
+                "prompt": prompt_summary,
+                "input": user_input,
+                "completion": completion_summary
+            }
+            data.append(item_summary)
+
+            item_keyphrase = {
+                "prompt": prompt_keyphrase,
+                "input": user_input,
+                "completion": completion_keyphrase
+            }
+            data.append(item_keyphrase)
+
+            item_senti = {
+                "prompt": prompt_senti,
+                "input": user_input,
+                "completion": completion_senti
+            }
+            data.append(item_senti)
+
+        else:
+            raise ValueError("Invalid mode specified")
+
+    workbook.close()
+
+    # random.shuffle(data)
+
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
