8efd49efee5707ef2e603e4bcd4aec0761ee5277
ps-penguin <58096499+ps-penguin@users.noreply.github.com>
2023-12-12 18:21:15 +0900

    Update summary.py

--- backend/service/summary.py
+++ backend/service/summary.py
@@ -20,9 +20,9 @@ DEFAULT_EOS_TOKEN = "</s>"
 DEFAULT_BOS_TOKEN = "</s>"
 DEFAULT_UNK_TOKEN = "</s>"
 
-SUM_TRINITY_PATH = 'model/summary/test_trinity_dataver4_12_4_1e-4/checkpoint-7500'
-SA_KOGPT2_PATH = 'model/summary/kogpt2_sentiment'
-KEYWORD_TRINITY_PATH = 'model/summary/keyword_trinity_10/checkpoint-3000'
+SUM_KOGPT_PATH = 'model/summary/test_trinity_dataver4_12_4_1e-4/checkpoint-7500'
+SA_KOGPT_PATH = 'model/summary/kogpt2_sentiment'
+KEYWORD_KOGPT_PATH = 'model/Keyphrase'
 TOTAL_KOGPT_PATH = 'model/TotalKogpt'
 
 class SummaryGenerator():
